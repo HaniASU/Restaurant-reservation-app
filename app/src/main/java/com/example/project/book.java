ea4876e6463c7f33c85cca2ce64481c786d99d55
ahmed1060 <ahmed.hossam1060@gmail.com>
2022-08-25 13:27:06 +0200

    book v3

--- app/src/main/java/com/example/project/book.java
+++ app/src/main/java/com/example/project/book.java
@@ -3,6 +3,11 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+
+import com.example.project.data.databasesqlite;
 
 public class book extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class book extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_book);
+    }
+    public void save_info(View view)
+    {
+        databasesqlite b= new databasesqlite(this);
+
+        EditText numberofpersons = (EditText) findViewById(R.id.num_of_persons);
+        EditText dateofbooking = (EditText) findViewById(R.id.date_of_booking);
     }
 }
