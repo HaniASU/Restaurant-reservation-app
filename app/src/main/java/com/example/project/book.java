bff0923cf293bbf32cd5478dc8aa3877542bb8ea
ahmed1060 <ahmed.hossam1060@gmail.com>
2022-08-25 13:40:09 +0200

    book v3.1

--- app/src/main/java/com/example/project/book.java
+++ app/src/main/java/com/example/project/book.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.project.data.databasesqlite;
 
@@ -25,5 +26,21 @@ public class book extends AppCompatActivity {
 
         EditText numberofpersons = (EditText) findViewById(R.id.num_of_persons);
         EditText dateofbooking = (EditText) findViewById(R.id.date_of_booking);
+
+        String NumberOfPersons = numberofpersons.getText().toString();
+        String DateOfBooking = dateofbooking.getText().toString();
+
+        if ( NumberOfPersons.isEmpty()|| DateOfBooking.isEmpty() )
+        {
+            Toast.makeText(this, "Invaild data", Toast.LENGTH_LONG).show();
+        }
+        else {
+            String result = b.insert_booking_info(Integer.parseInt(NumberOfPersons) ,DateOfBooking);
+            Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+
+            numberofpersons.getText().clear();
+            dateofbooking.getText().clear();
+
+        }
     }
 }
